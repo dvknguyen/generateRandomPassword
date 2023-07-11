5da0854035cf2a770a81487c8d2b44196775bc42
Duy <duy.van.nguyen@protonmail.com>
2023-07-12 00:15:03 +0200

    Fix error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Slider from "@mui/material/Slider";
 import Switch from "@mui/material/Switch";
 import TextField from "@mui/material/TextField";
